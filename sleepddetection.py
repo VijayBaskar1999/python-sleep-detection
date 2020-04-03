44c4851bff289f6cb122ae38c34364eb276fa7af
VijayBaskar1999 <42474466+VijayBaskar1999@users.noreply.github.com>
2020-04-03 22:35:07 +0530

    Add files via upload

---
+++ sleepddetection.py
@@ -0,0 +1,100 @@
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+import cv2
+from playsound import playsound
+
+
+
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
+
+def mouth_aspect_ratio(mouth):
+    A = distance.euclidean(mouth[2], mouth[10])
+    B = distance.euclidean(mouth[4], mouth[8])
+    C = distance.euclidean(mouth[0], mouth[6])
+    mou = (A + B) / (2.0 * C)
+    return mou
+def jaw_left_distance(jaw):
+    A = distance.euclidean(jaw[0], jaw[8])
+    return A
+def jaw_right_distance(jaw):
+    A = distance.euclidean(jaw[8], jaw[16])
+    return A
+def difference_in_jaw(a,b):
+    c = abs(a-b)
+    return c
+
+thresh = 0.25
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("model/shape_predictor_68_face_landmarks.dat")  # Dat file is the crux of the code
+
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["mouth"]
+(jStart, jEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["jaw"]
+cap = cv2.VideoCapture(0)
+flag = 0
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    for subject in subjects:
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)  # converting to NumPy Array
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        mouth = shape[mStart:mEnd]
+        jaw = shape[jStart:jEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        mouthEAR = mouth_aspect_ratio(mouth)
+        jaw_L_Result = jaw_left_distance(jaw)
+        jaw_R_Result = jaw_right_distance(jaw)
+        jaw_diff = difference_in_jaw(jaw_L_Result, jaw_R_Result)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        mouthHull = cv2.convexHull(mouth)
+        jawHull = cv2.convexHull(jaw)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [jawHull], -1, (0, 255, 0), 1)
+        cv2.putText(frame, "ear:{:.2f}".format(ear), (10, 50),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "ear:{:.2f}".format(mouthEAR), (10, 80),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "L_dis:{:.2f}".format(jaw_L_Result), (10, 110),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "R_dis:{:.2f}".format(jaw_R_Result), (200, 110),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "J_Dif:{:.2f}".format(jaw_diff), (10, 140),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        if ear < thresh:
+            flag += 1
+            print(flag)
+            if flag >= frame_check:
+               # playsound('alert.mp3')
+                cv2.putText(frame, "****************ALERT!****************", (10, 30),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(frame, "****************ALERT!****************", (10, 325),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            # print ("Drowsy")
+        else:
+            flag = 0
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+cv2.destroyAllWindows()
+cap.release()
